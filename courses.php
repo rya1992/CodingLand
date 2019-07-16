82fd0f6fa5a6bb34a97c8a2b77d1f4110a31598a
rya1992 <39709767+rya1992@users.noreply.github.com>
2019-07-16 09:56:38 +0300

    add php files

---
+++ courses.php
@@ -0,0 +1,71 @@
+<?php include('server.php') ?>
+<!DOCTYPE html>
+ <html class="no-js">
+    <head>
+        // لربط الملفين
+        <link rel="stylesheet" href="Untitled-3.css">
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title> الدورات</title>
+        <meta name="viewport" content="width=device-width">
+		<link rel="icon" type="image/png" href="horus-favicon.png" />
+        <!-- working with sass -> scss -->
+        <link rel="stylesheet" href="css/style.css">
+		<link href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+		<script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min.js"></script>
+
+    </head>
+    <body>
+        <header class="header">
+            <div class="container">
+                <nav class="row">
+                    <ul class="col12">
+                        <li><a href="#home"><i class="fa fa-code"></i>تطوير التطبيقات للأطفال </a></li>
+                        <li><a href="#tags"><i class="fa fa-code"></i> SCRATCH</a></li>
+                        <li><a href="#code"><i class="fa fa-code"></i> YOUTUBE CREATOR </a></li>
+                        <li><a href="#contact"><i class="fa fa-code"></i> ابدأ تطبيقك المبتكر  </a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+
+        <section id="home" class="item">
+            <div class="container">
+                <h1> تطوير التطبيقات للأطفال</h1>
+                <p> في السنة الدراسية الأولى -مرحلة المبتدئين سوف يكون التركيز على إطلاق لعبتك وتطبيقك الأول.</p>
+                <p> من أجل أن تصبح مطورا للتطبيقات، ستتعلم كيفية تطوير فكرة تطبيق، وكيفية استخدام أدوات التطوير، وكيفية إنشاء الواجهات، وكيفية تنفيذ الكائنات المختلفة، والرسوم المتحركة والأصوات واستخدام الوظائف الحسابية</p>
+              <p>تتضمن هذه السنة مكونات MyFirstApp (تطبيقي الأول) وMyFirstGame (لعبتي الأولى)، حيث ستتعلم وتنتج كلا من الألعاب والتطبيقات الخاصة بأفكارك وإبداعاتك.</p>
+            </div> <!-- End container -->
+        </section>
+
+        <section id="tags" class="item">
+            <div class="container">
+                <h1> SCRATCH</h1>
+                  <p>تجذب هذه الدورة التدريبية التمهيديّة الأطفال نحو إنشاء الألعاب ومقاطع الفيديو والقصص، بينما يتعلّمون أسس البرمجة؛ ومن ثمّ فليست هناك حاجة إلى وجود خبرات سابقة. </p>
+                   <p> وسوف يتعرّف الأطفال على بيئة تعلّم Scratch، والشخصيات الرسومية المتحركة الجميلة لها وأدوات التصميم الرسومي الرائعة.</p>
+                   <p>وسوف ينشئون ألعابًا مرحة بواسطة سحب وإفلات الكتل البرمجية المدمجة التي ستساعدهم على تطبيق المنطق الداخل وهيكل البرمجة.</p>
+            </div>
+        </section>
+
+        <section id="code" class="item">
+            <div class="container">
+              <h1> YOUTUBE CREATOR </h1>
+              <p>أصبح توثيق اللحظات الخالدة ومشاركة القصص اليوميّة على وسائل التواصل الاجتماعي جزءًا من الروتين اليومي في حياتنا. ولكن، هل يمكن لهذا النشاط الاعتيادي أن يتحوّل (بقليل من المساعدة والتوجيه) إلى تعبير فنّي؟</p>
+                <p>تزوّد دورتنا التدريبيّة "YouTube Creator" الأطفال بالأدوات التي تمكّنهم من التعبير عن أنفسهم تعبيرًا فنيًّا احترافيًّا.</p>
+                <p> فالطفل حين ينتج فيديوهات الشخصية، فهو يكتسب الثقة ويعزّز هويّته ويكتشف أساليب إنتاجية ملائمة للتعبير عن عواطفه. وإذا أحب أطفالك وسائل التواصل الاجتماعي وكنت ترغب في إثرائهم وتزويدهم بمجموعة الأدوات من خلال نشاط مرح وابتكاري، فإن هذه الدورة التدريبيّة هي سبيلك لتحقيق ذلك.</p>
+
+          </div>
+        </section>
+
+        <section id="contact" class="item">
+            <div class="container">
+                <h1>ابدأ تطبيقك المبتكر</h1>
+               <p>خلال السنة الدراسية الأخيرة -سنة التخرج, سيتعلم الطلاب كيفية تصميم التطبيقات للأجهزة المحمولة، مما يكسبهم تجربة مثالية ومحاكاة حقيقية للفرق الناشئة لإطلاق تطبيقاتهم الجديدة المتقدمة.</p>
+               <p>لن يطوروا فقط المهارات التكنولوجية، بل سيطورون أيضا خصائص أخرى مثل الإبداع والمثابرة والمهارات الاجتماعية اللازمة لتكوين الفرق الرائعة.</p>
+            </div>
+        </section>
+
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+        <script src="js/script.js"></script>
+    </body>
+</html>
---
+++ server.php
@@ -0,0 +1,53 @@
+<?php
+
+// initializing variables
+$username = "";
+$email    = "";
+
+$errors = array();
+
+// connect to the database
+$db = mysqli_connect('localhost', 'root', '', 'registration');
+
+// REGISTER USER
+if (isset($_POST['reg_user'])) {
+  // receive all input values from the form
+  $username = mysqli_real_escape_string($db, $_POST['username']);
+  $email = mysqli_real_escape_string($db, $_POST['email']);
+
+
+  // form validation: ensure that the form is correctly filled ...
+  // by adding (array_push()) corresponding error unto $errors array
+  if (empty($username)) { array_push($errors, "Username is required"); }
+  if (empty($email)) { array_push($errors, "Email is required"); }
+
+
+    // first check the database to make sure
+  // a user does not already exist with the same username and/or email
+  $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' LIMIT 1";
+  $result = mysqli_query($db, $user_check_query);
+  $user = mysqli_fetch_assoc($result);
+
+  if ($user) { // if user exists
+    if ($user['username'] === $username) {
+      array_push($errors, "Username already exists");
+    }
+
+    if ($user['email'] === $email) {
+      array_push($errors, "email already exists");
+    }
+  }
+
+  // Finally, register user if there are no errors in the form
+
+
+  	$query = "INSERT INTO users (username, email) 
+  			  VALUES('$username', '$email')";
+mysqli_query($db, $query);
+  
+
+}
+
+
+
+?>
